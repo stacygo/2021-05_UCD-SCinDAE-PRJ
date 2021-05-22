47af8de230fab887ac431824b2c43a98c75d65bd
Anastasia Gorbunova <stacy.gorbunova@gmail.com>
2021-05-22 23:36:22 +0100

    Added the exploration of parser results.

--- parser/explore_parser.py
+++ parser/explore_parser.py
@@ -1,4 +1,29 @@
+from functions import print_pretty_table, print_pretty_list, show_extended_info
 import pandas as pd
+import numpy as np
 
 if __name__ == '__main__':
-    parser_marks_df = pd.read_csv('../parser/output/parser_marks_df.csv')
+    df = pd.read_csv('output/parser_marks_df.csv',
+                     parse_dates=['date'],
+                     dtype={'mark': np.float64, 'max_mark': np.float64, 'year': np.float64})
+
+    # Explore parser output: show dataframe info
+    print('\nShow dataframe info\n')
+    print(print_pretty_table(show_extended_info(df)))
+
+    # View the first 5 rows
+    print('\nView the first 5 rows\n')
+    print(print_pretty_table(df.iloc[:, 0:8].head()))
+    print(print_pretty_table(df.iloc[:, 8:].head()))
+
+    # Check unique values in 'category', 'county', 'criteria'
+    choose_cols = ['category', 'county', 'criteria']
+    output_pt = [['column', 'unique values']]
+    for col in choose_cols:
+        show_col_unique = df[col].unique()
+        show_col_unique = sorted(show_col_unique)
+        show_col_unique = print_pretty_list(show_col_unique)
+        output_pt.append([col, show_col_unique])
+
+    print('\nheck unique values in \'category\', \'county\', \'criteria\'\n')
+    print(print_pretty_table(output_pt))
