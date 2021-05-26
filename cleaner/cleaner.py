07f2c7b54c5f218b12abd227ea92ac9f47a3bad4
Anastasia Gorbunova <stacy.gorbunova@gmail.com>
2021-05-27 00:20:28 +0100

    Updated data cleaning process for dates: – replaced filling in N/As with dropping, - added 'suspicious' dates report.

--- cleaner/cleaner.py
+++ cleaner/cleaner.py
@@ -47,13 +47,45 @@ if __name__ == '__main__':
     towns_df = pd.read_csv('input/towns.csv')
     df = df.merge(towns_df, how='left', on=['county_l1', 'town'])
 
+    # Check & clean 'date' column
+
+    # > Step 1: Add a column with a month
+    df['date_month'] = df['date'].dt.month
+
+    # > Step 2: Check the min / max / pd.NaT dates by years
+    find_criteria_total = df['criteria'] == 'TOTAL MARK'
+    output_df = df[find_criteria_total].groupby('year').agg(
+        date_count=('date', len),
+        date_min=('date', np.min),
+        date_max=('date', np.max),
+        date_nan=('date', lambda x: x.isnull().sum())
+    )
+    output_df['date_min'] = output_df['date_min'].dt.date
+    output_df['date_max'] = output_df['date_max'].dt.date
+    print('\nShow min / max / pd.NaT dates by years\n')
+    print(print_pretty_table(output_df))
+
+    # > Step 3: Make a csv file with a list of towns with the adjudication date out of May-September
+    find_month_in = df['date_month'].isin([5, 6, 7, 8, 9])
+    choose_cols = ['date', 'county_l1', 'town', 'pdf_path', 'pdf_name']
+    output_df = df[~find_month_in][choose_cols].drop_duplicates()
+    output_df = output_df.dropna(subset=['date']).sort_values(by=choose_cols).reset_index(drop=True)
+    output_df['date'] = output_df['date'].dt.date
+    print('\nShow a list of towns with the adjudication date out of May-September\n')
+    print(print_pretty_table(output_df.head()))
+
+    write_df_to_csv(output_df, 'output/cleaner_dates_df.csv')
+
+    # > Step 4: Drop the rows without the date
+    df = df.dropna(subset=['date'])
+
     # Check & clean 'criteria' column
 
     # > Step 1: Check the total mark dynamics by years and counties
-    find_total_mark = df['criteria'] == 'TOTAL MARK'
+    find_criteria_total = df['criteria'] == 'TOTAL MARK'
     output_df = \
-        df[find_total_mark].pivot_table(index=['county'], columns=['year'], values='max_mark',
-                                        aggfunc=np.mean, fill_value=0, margins=True, margins_name='(total)')
+        df[find_criteria_total].pivot_table(index=['county'], columns=['year'], values='max_mark',
+                                            aggfunc=np.mean, fill_value=0, margins=True, margins_name='(total)')
     print('\nShow mean (total) max_mark by years\n')
     print(print_pretty_table(output_df.loc[['(total)']].T))
 
@@ -73,11 +105,8 @@ if __name__ == '__main__':
     criteria_df = pd.read_csv('input/criteria_2014.csv')
     df = df.merge(criteria_df, how='left', on=['criteria'])
 
-    # Check & clean 'date' column
-    df['date'] = df['date'].fillna(pd.to_datetime({'year': df['year'], 'month': 9, 'day': 1}))
-
     # Drop some columns, and drop duplicates
-    choose_cols = ['county', 'town', 'criteria', 'pdf_name', 'pdf_path', 'pdf_path_county']
+    choose_cols = ['county', 'town', 'criteria', 'pdf_name', 'pdf_path', 'pdf_path_county', 'date_month']
     df = df.drop(choose_cols, axis=1).drop_duplicates()
 
     # Explore cleaner output: show dataframe info
